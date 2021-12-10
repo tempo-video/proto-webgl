b522224fd393a77145250c5335e45ac802655441
paul <pcotogno13@gmail.com>
2021-12-10 10:58:27 +0100

    10/12

--- apps/proto-webgl/src/app/app.element.ts
+++ apps/proto-webgl/src/app/app.element.ts
@@ -14,6 +14,11 @@ import {
   Camera,
   Clock,
   Object3D,
+  Vector3,
+  Box3,
+  Vector,
+  PlaneHelper,
+  Plane,
 } from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
@@ -50,6 +55,8 @@ import { addShapeThree } from './shapes/addShapeThree';
 import { getAllVideos } from './videos/loadAllVideos';
 import { playVideo } from './videos/controls';
 
+import { localPlanes } from './fonts/addTextThree';
+
 export { scene, camera };
 
 const dataEditor = data.editor;
@@ -69,6 +76,9 @@ const Scenes: sceneBuffer[] = [];
 
 let animations: any[] = [];
 
+import * as TWEEN from '@tweenjs/tween.js';
+import { posix } from 'path';
+
 ////////////////////////////////////////////////////////
 ////////////////Initialisation de THREE/////////////////
 ////////////////////////////////////////////////////////
@@ -78,7 +88,7 @@ async function init() {
   videoHeight = dataEditor.videoHeight;
 
   scene = new Scene();
-  scene.background = new Color(0xfff0ff);
+  scene.background = new Color(0x0f00ff);
 
   camera = new OrthographicCamera(
     videoWidth / -2,
@@ -93,6 +103,7 @@ async function init() {
 
   renderer = new WebGLRenderer({ antialias: true });
   renderer.setSize(videoWidth * 0.4, videoHeight * 0.4);
+  renderer.localClippingEnabled = true;
   document.getElementById('renderer')!.appendChild(renderer.domElement);
 
   const Quad = new Mesh(
@@ -103,7 +114,7 @@ async function init() {
   Quad.position.set(0, 0, -2);
   Quad.rotation.set(Math.PI, 0, Math.PI);
 
-  scene.add(Quad);
+  //scene.add(Quad);
 
   // -> load all the assets
   await getAllVideos(dataEditor.tracks[0].itemIds, dataEditor.trackItems);
@@ -113,23 +124,23 @@ async function init() {
   // -> Add them in threejs
   addAllVideosThree();
   getAllElements();
-  addTextThree('salutttt\ntoua', 'regular');
-  addSubtitles(
+  addTextThree('Bienvenue\nsur le site\nTEMPO', 'regular');
+  /*addSubtitles(
     'In literary theory, a text is any object that can be "read", whether this object is a work of literature, a street sign, an arrangement of buildings on a city block, or styles of clothing. It is a coherent set of signs that transmits some kind of informative message.',
     allFontsLoaded[1].font,
     scene,
     0.7,
     1920 * 0.5,
     1080 / 2
-  );
-  playVideo('ede08c9d-2483-454d-8478-478c32cfc7d5', 0, Scenes);
+  );*/
+  //playVideo('ede08c9d-2483-454d-8478-478c32cfc7d5', 0, Scenes);
   ChooseElement(Elements);
   ChooseTransition();
 
   // -> composer pour les effets globaux
   Composer();
 
-  animation();
+  animation(null);
 
   ////////////////////////////////////////////////////////////////
   ///////////////////////// GUI Integration //////////////////////
@@ -180,12 +191,14 @@ function Composer() {
 
 var onlyOnce = true;
 
-function animation() {
+function animation(time: any) {
   requestAnimationFrame(animation);
   const deltaTime = clock.getElapsedTime();
 
-  if (scene.getObjectByName('Tempo') && onlyOnce) {
-    var obj = scene.getObjectByName('Tempo');
+  if (scene.children[1] && onlyOnce) {
+    var obj = {
+      objects: scene.children[1],
+    };
 
     animations.push(obj);
 
@@ -194,10 +207,14 @@ function animation() {
     onlyOnce = false;
   }
 
+  TWEEN.update(deltaTime);
+
   let dt = clock.getDelta();
   animations.forEach((object) => {
-    object.updateAnimation(deltaTime);
+    object.updateAnimation(deltaTime, dt);
   });
+
+  TWEEN.update(time);
 
   //Scenes[2].render();
   //Scenes[1].render();
@@ -207,63 +224,318 @@ function animation() {
 }
 
 function onlyOnceF(obj: any) {
-  /*cadre.onClick = function (intersection) {
-    if (this.animationStatus == 0) {
-      this.animationStatus = 1;
-    } else if (this.animationStatus == 2) {
-      this.animationStatus = 3;
-    }
-  };*/
-
   console.log('onlyOnceSet');
 
-  obj.animationStatus = 0;
+  obj.options = {
+    animationStatus: 0,
+    end: -150,
+    start: 300,
+    duration: 50,
+    i: -10,
+    oldAngle: 0,
+    onlyOnce: true,
+  };
+
+  console.log('obj : ' + obj);
 
   document.addEventListener('click', () => {
     console.log('eoh');
-    if (obj.animationStatus == 0) {
+    if (obj.options.animationStatus == 0) {
       console.log('obj anim set to 1');
-      obj.animationStatus = 1;
-    } else if (obj.animationStatus == 2) {
-      console.log('obj anim set to 3');
-      obj.animationStatus = 3;
+      obj.options.animationStatus = 3;
     }
   });
 
-  obj.end = 200;
-  obj.start = 0;
-  obj.duration = 1000;
-  obj.updateAnimation = function (dt: any) {
-    let angle = (dt * (this.end - this.start)) / this.duration;
-    switch (this.animationStatus) {
-      case 1:
-        this.position.y -= angle;
-        //this.position.y = easeOutBack(map_range(this.position.y - angle, this.end, this.start, 0, 1));
-        if (this.position.y < this.start) {
-          this.position.y = this.start;
-          this.animationStatus = 2;
+  obj.objects.position.set(0,0,0);
+
+  
+  new Box3()
+    .setFromObject(obj.objects)
+    .getCenter(obj.objects.position)
+    .multiplyScalar(-1);
+
+  /*obj.objects.position.set(0,0,0);
+  obj.objects.visibility = true;
+  /*var mesh = obj.objects;
+  var center = new Vector3();
+  mesh.geometry.computeBoundingBox();
+  mesh.geometry.boundingBox.getCenter(center);
+  mesh.geometry.center();
+  mesh.position.copy(center);*/
+
+  obj.updateAnimation = function (dt: any, d: any) {
+    switch (this.options.animationStatus) {
+      case 2:
+        if (this.options.onlyOnce) {
+          let position: { x: number; y: number; z: number } = {
+            x: 0,
+            y: 0,
+            z: 0,
+          };
+          new TWEEN.Tween(position)
+            .to({ x: 0, y: -1200, z: 0 }, 1500)
+            .easing(TWEEN.Easing.Back.InOut)
+            .onUpdate(() => {
+              this.objects.position.y = position.y;
+            })
+            .onComplete(() => {
+              this.options.onlyOnce = true;
+              this.options.animationStatus = 0;
+            })
+            .start();
+          this.options.onlyOnce = false;
         }
         break;
       case 3:
-        this.position.y += angle;
-        if (this.position.y > this.end) {
-          this.position.y = this.end;
-          this.animationStatus = 0;
+        if (this.options.onlyOnce) {
+          let scale: { x: number; y: number; z: number } = { x: 1, y: 1, z: 1 };
+          console.log('scale : ' + scale);
+          new TWEEN.Tween(scale)
+            .to({ x: 1.8, y: 1.8, z: 1.8 }, 600)
+            .easing(TWEEN.Easing.Elastic.Out)
+            .onUpdate(() => {
+              console.log('update scale y: ' + scale.y);
+              this.objects.scale.set(scale.x, scale.y, scale.z);
+            })
+            .onComplete(() => {
+              this.options.animationStatus = 0;
+              this.options.onlyOnce = true;
+            })
+            .start();
+          this.options.onlyOnce = false;
+        }
+        break;
+      case 4:
+        console.log('essai lancé case 4');
+        if (this.options.onlyOnce) {
+          console.log('case 4 start');
+          let opacity: { o: number } = { o: 1 };
+          new TWEEN.Tween(opacity)
+            .to({ o: 0 }, 1000)
+            .easing(TWEEN.Easing.Quartic.In)
+            .onUpdate(() => {
+              console.log('update opacity, o : ' + opacity.o);
+              this.objects[1].material.opacity = opacity.o;
+            })
+            .onComplete(() => {
+              this.options.onlyOnce = true;
+              this.options.animationStatus = 0;
+            })
+            .start();
+          this.options.onlyOnce = false;
+        }
+        break;
+      case 5:
+        if (this.options.onlyOnce) {
+          let start = {
+            positions: { x: 0, y: 400, z: 0 },
+            opacity: 0,
+            scale: { x: 1, y: 1, z: 1 },
+          };
+          let end = {
+            positions: { x: 0, y: -180, z: 0 },
+            opacity: 1,
+            scale: { x: 1.2, y: 1.2, z: 1.2 },
+          };
+          for (let i = 0; i < this.objects.children.length; i++) {
+            new TWEEN.Tween(start)
+              .to(end, 600)
+              .delay(50 * i)
+              .easing(TWEEN.Easing.Cubic.InOut)
+              .onStart(() => {
+                this.objects.visible = true;
+                let box = new Box3().setFromObject(this.objects.children[i]);
+                let height = box.max.y - box.min.y;
+                console.log(
+                  'top plane : ' +
+                    (-180 + height * i) +
+                    'bottome plane : ' +
+                    (-180 + height * (i + 1))
+                );
+
+                this.objects.children[i].children.forEach((element: any) => {
+                  element.material.clippingPlanes = [
+                    new Plane(new Vector3(0, 1, 0), 200 + height / 2 - 200 * i),
+                    new Plane(
+                      new Vector3(0, -1, 0),
+                      -200 - height / 2 + 200 * (i + 1)
+                    ),
+                  ];
+                });
+              })
+              .onUpdate(() => {
+                this.objects.children[i].position.set(
+                  start.positions.x,
+                  start.positions.y + i * 200,
+                  start.positions.z
+                );
+              })
+              .start()
+              .chain(
+                new TWEEN.Tween(end)
+                  .to(
+                    {
+                      positions: { x: -300, y: -180, z: 0 },
+                      opacity: 1,
+                      scale: { x: 1.8, y: 1.8, z: 1.8 },
+                    },
+                    500
+                  )
+                  .easing(TWEEN.Easing.Cubic.InOut)
+                  .onStart(() => {
+                    /*new TWEEN.Tween(end)
+                      .to(
+                        {
+                          positions: { x: 0, y: -150, z: 0 },
+                          opacity: 1,
+                          scale: { x: 1.3, y: 1.3, z: 1.3 },
+                        },
+                        500
+                      )
+                      .easing(TWEEN.Easing.Cubic.InOut)
+                      .onUpdate(() => {
+                        if (i == 2) {
+                          this.objects[i].position.set(
+                            end.positions.x,
+                            end.positions.y + (i * 200),
+                            end.positions.z
+                          );
+                        }
+                      })
+                      .start();*/
+                    new TWEEN.Tween(end)
+                      .to(
+                        {
+                          positions: { x: 0, y: -1250, z: 0 },
+                          opacity: 1,
+                          scale: { x: 1.3, y: 1.3, z: 1.3 },
+                        },
+                        700
+                      )
+                      .delay(1000)
+                      .easing(TWEEN.Easing.Cubic.InOut)
+                      .onUpdate(() => {
+                        this.objects.children[i].position.set(
+                          end.positions.x,
+                          end.positions.y + i * 200,
+                          end.positions.z
+                        );
+                      })
+                      .onComplete(() => {
+                        this.objects.visible = false;
+                        this.objects.children[i].children.forEach(
+                          (elements: any) => {
+                            elements.material.clippingPlanes = [];
+                          }
+                        );
+                        this.options.animationStatus = 0;
+                        this.options.onlyOnce = true;
+                      })
+                      .start();
+                  })
+                  .onUpdate(() => {
+                    if (i == 2) {
+                      this.objects.children[i].scale.set(
+                        end.scale.x,
+                        /*end.scale.y*/ 1.3,
+                        /*end.scale.z*/ 1.3
+                      );
+                    }
+                  })
+              );
+
+            new TWEEN.Tween(start)
+              .to(end, 600)
+              .delay(50 * i)
+              .easing(TWEEN.Easing.Quintic.In)
+              .onUpdate(() => {
+                //this.objects[i].material.opacity = start.opacity;
+              })
+              .start();
+            new TWEEN.Tween(start)
+              .to(end, 600)
+              .delay(50 * i)
+              .easing(TWEEN.Easing.Quintic.Out)
+              .onUpdate(() => {
+                this.objects.children[i].scale.set(
+                  start.scale.x,
+                  start.scale.y,
+                  start.scale.z
+                );
+              })
+              .start();
+          }
+          this.options.onlyOnce = false;
+        }
+        break;
+      case 6:
+        if (this.options.onlyOnce) {
+          let texte = this.objects[0];
+          texte.material.opacity = 1;
+          texte.position.set(0, 0, 0);
+          texte.scale.set(1, 1, 1);
+
+          let number = 7;
+          let height = 1080;
+          let taillerepet = height / number + 30;
+
+          let posClones: { x: number; y: number; z: number }[] = [];
+
+          for (
+            let i = -Math.round(number / 2);
+            i < Math.round(number / 2);
+            i++
+          ) {
+            let pos = { x: 0, y: texte.position.y + taillerepet * i, z: 0 };
+            i !== 0 ? posClones.push(pos) : console.log('0');
+          }
+
+          let scale = { scale: 1.3 };
+
+          let clones: any[] = [];
+
+          for (let i = 0; i < posClones.length; i++) {
+            let clone = texte.clone(false);
+            new TWEEN.Tween(scale)
+              .to({ scale: 1 }, 50)
+              .delay(50 * i)
+              .easing(TWEEN.Easing.Exponential.Out)
+              .onStart(() => {
+                clone.position.set(
+                  posClones[i].x,
+                  posClones[i].y,
+                  posClones[i].z
+                );
+                clone.scale.set(scale.scale, scale.scale, scale.scale);
+                clones.push(clone);
+                scene.add(clone);
+              })
+              .onUpdate(() => {
+                clone.scale.set(scale.scale, scale.scale, scale.scale);
+              })
+              .chain(
+                new TWEEN.Tween({})
+                  .to({}, 100)
+                  .delay(800 - 25 * i)
+                  .onComplete(() => {
+                    this.options.animationStatus = 0;
+                    this.options.onlyOnce = true;
+                    scene.remove(clone);
+                  })
+              )
+              .start();
+          }
+          this.options.onlyOnce = false;
+        }
+        break;
+      case 7:
+        if (this.options.onlyOnce) {
+          this.options.onlyOnce = false;
         }
         break;
     }
   };
-}
-
-function easeOutBack(x: number): number {
-  const c1 = 1.70158;
-  const c3 = c1 + 1;
-  
-  return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
-  }
-
-function map_range(value: any, low1: any, high1: any, low2: any, high2: any) {
-  return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
+  console.log(obj);
 }
 
 ////////////////////////////////////////////////////////////////
@@ -413,7 +685,29 @@ async function FontsLoader() {
 }
 
 function addTextThree(textString: string, type: string) {
-  scene.add(TextLoader(textString, type, allFontsLoaded));
+  let texts = TextLoader(textString, type, allFontsLoaded);
+  let ligneEspaceIndex = 0;
+  texts.children.forEach((element) => {
+    let lastMaxX = 0;
+    element.children.forEach((elem) => {
+      if (elem.name === ' ') {
+        elem.position.set(lastMaxX + 40, 0, 0);
+        lastMaxX = elem.position.x + 20;
+      } else {
+        console.log(elem);
+        let box = new Box3().setFromObject(elem);
+        elem.position.set(lastMaxX + 20, 0, 0);
+        lastMaxX = elem.position.x + (box.max.x - box.min.x);
+      }
+      new Box3()
+          .setFromObject(elem)
+          .getCenter(elem.position)
+          .multiplyScalar(1);
+    });
+    element.position.set(0, ligneEspaceIndex * 150, 0);
+    ligneEspaceIndex++;
+  });
+  scene.add(texts);
 }
 
 /////////////////////////////////////////////////////////////////
--- apps/proto-webgl/src/app/fonts/addTextThree.ts
+++ apps/proto-webgl/src/app/fonts/addTextThree.ts
@@ -5,33 +5,71 @@ import {
   MeshBasicMaterial,
   DoubleSide,
   Mesh,
+  Object3D,
+  Plane,
+  Vector3,
+  Group,
 } from 'three';
+
+export const localPlanes = [
+  new Plane(new Vector3(0, -1, 0), 0),
+  new Plane(new Vector3(-1, 0, 0), 0.1),
+];
 
 function TextLoader(
   textString: string,
   type: string,
   allFontsLoaded: LoadedFont[]
 ) {
-  const fontIndex = allFontsLoaded.findIndex((element) => {
-    return element.name === type;
+  let lines = textString.split('\n');
+  let letters = textString.split('');
+
+  let linesL: string[][] = [];
+  let index = 0;
+  letters.forEach((element) => {
+    if (element === '\n') {
+      index++;
+    } else {
+      if (!linesL[index]) {
+        linesL.push([]);
+      }
+      linesL[index].push(element);
+    }
   });
-  const textGeo = new TextGeometry(textString, {
-    size: 100,
-    height: 0,
-    curveSegments: 6,
-    font: allFontsLoaded[fontIndex].font,
-  });
-  const color = new Color();
-  color.setRGB(255, 255, 255);
-  const textMaterial = new MeshBasicMaterial({
-    color: color,
-    side: DoubleSide,
-  });
-  const text = new Mesh(textGeo, textMaterial);
-  text.name = type; // Changer sur le id du texte
-  text.position.set(-200, 0, 0);
-  text.rotation.set(Math.PI, 0 ,0);
-  return text;
+  console.log(linesL);
+
+  const textGroup = new Group();
+  linesL.forEach(element => {
+    const lineGroup = new Group();
+    element.forEach(elem => {
+      const fontIndex = allFontsLoaded.findIndex((element) => {
+        return element.name === type;
+      });
+      const textGeo = new TextGeometry(elem, {
+        size: 100,
+        height: 0,
+        curveSegments: 6,
+        font: allFontsLoaded[fontIndex].font,
+      });
+      const color = new Color();
+      color.setRGB(255, 255, 255);
+      const textMaterial = new MeshBasicMaterial({
+        color: color,
+        side: DoubleSide,
+      });
+      const text = new Mesh(textGeo, textMaterial);
+      text.name = elem; // Changer sur le id du texte
+      text.position.set(-200, 0, 0);
+      text.rotation.set(Math.PI, 0, 0);
+      lineGroup.add(text);
+    })
+    textGroup.add(lineGroup);
+  })
+
+
+  console.log(textGroup);
+
+  return textGroup;
 }
 
 export { TextLoader };
